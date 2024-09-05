27e03f075b9d9310b99bc1f73244726d2a8fc2e3
unknown <suporte@SUPORTE2.batista.local>
2024-09-05 10:31:28 -0300

    clase codec

--- src/main/java/org/example/codec/VideoCodec.java
+++ src/main/java/org/example/codec/VideoCodec.java
@@ -1,4 +1,12 @@
 package org.example.codec;
-
+import  java.util.ArrayList;
+import java.util.List;
 public class VideoCodec {
-}
+    private List<VideoClip> clips;        // Lista de clipes de vídeo;
+    public VideoCodec(){
+        // Construtor que inicializa as listas
+        clips = new ArrayList<>();}
+    public void addClip(VideoClip clip){ //clip porque se refere a um. O arquivo.
+        clips.add(clip);
+        // Esse metodo adiciona um clip de vídeo à lista utilizando clips.add (a biblioteca do java.util)
+}}
